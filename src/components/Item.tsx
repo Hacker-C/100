7b10334712c1d584667be9f13c25d509434170ba
Hacker-C <1507559148@qq.com>
2022-12-03 16:43:19 +0800

    🐛 fix: fix module location error

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,10 +1,10 @@
-import type { AppItem } from '@/App'
+import type { AppItem } from '@/apps/Home'
 
 function Item(props: AppItem) {
   const { id, title } = props
   return (
     <div mt-1 font-mono>
-      <span text-gray-400>{id}. </span>
+      <span text-gray-400>{ id }. </span>
       <span text-gray-600>{ title }</span>
     </div>
   )

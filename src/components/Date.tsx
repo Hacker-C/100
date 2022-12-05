f2a08b009d6230c231071642f1db1516e2cebc23
Hacker-C <1507559148@qq.com>
2022-12-05 21:20:23 +0800

    💄 style: adjust style

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,6 +1,6 @@
 export default function (props: { date: string }) {
   return (
-    <footer flex flex-row-reverse p-2>
+    <footer flex flex-row-reverse fixed right-2 bottom-1>
       <span font-mono text-gray-400>{ props.date }</span>
     </footer>
   )
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -6,7 +6,7 @@ interface AppItem {
 function Item(props: AppItem) {
   const { id, title } = props
   return (
-    <div mt-1 font-mono>
+    <div mt-1 font-mono whitespace-nowrap>
       <span text-gray-400>{ id }. </span>
       <span text-gray-600>{ title }</span>
     </div>
